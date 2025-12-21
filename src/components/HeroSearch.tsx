4ed59550663c61f8807405f5abf029b156bd4fd0
Avi-Mahatta <avinash@mahatta.com>
2025-12-22 00:02:10 +0530

    herosearch css issue

--- src/components/HeroSearch.tsx
+++ src/components/HeroSearch.tsx
@@ -1,16 +1,15 @@
 import { SearchIcon } from "./Icons";
 import SearchBox from "./search/SearchBox";
 import LocationSearch from "./search/LocationSearch";
-
 import SearchBtn from "./search/SearchBtn";
 
-
 export const HeroSearch = ({ mode }) => {
-  
   return(
-    <div className="max-w-4xl mx-auto bg-white/70 backdrop-blur-md border p-5 rounded-2xl shadow flex gap-3 relative animate-fadeInUp delay-300 z-50">
+    <div className="max-w-4xl mx-auto bg-white/70 backdrop-blur-md border p-5 rounded-2xl shadow flex flex-col md:flex-row gap-3 relative animate-fadeInUp delay-300 z-40">
       <LocationSearch disabled={mode === "readonly"} />
+
       <div className="hidden md:block w-px h-10 bg-slate-300" />
+
       <SearchBox disabled={mode === "readonly"} />
     </div>
   );
