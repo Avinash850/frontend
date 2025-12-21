103dba9bf1d7593f46ec3b3d1f96284a218a8ca3
Avi-Mahatta <avinash@mahatta.com>
2025-12-21 23:54:29 +0530

    mode for the stop

--- src/components/HeroSearch.tsx
+++ src/components/HeroSearch.tsx
@@ -5,14 +5,13 @@ import LocationSearch from "./search/LocationSearch";
 import SearchBtn from "./search/SearchBtn";
 
 
-export const HeroSearch =  () => {
-
-    return(
-        <div className="max-w-4xl mx-auto bg-white/70 backdrop-blur-md border border-slate-200 p-5 rounded-2xl shadow-2xl flex flex-col md:flex-row items-center gap-3 relative animate-fadeInUp delay-300 z-50">
-          <LocationSearch />
-          <div className="hidden md:block w-px h-10 bg-slate-300 " />
-          <SearchBox />
-          <SearchBtn/>
-        </div>
-    )
-}
+export const HeroSearch = ({ mode }) => {
+  
+  return(
+    <div className="max-w-4xl mx-auto bg-white/70 backdrop-blur-md border p-5 rounded-2xl shadow flex gap-3 relative animate-fadeInUp delay-300 z-50">
+      <LocationSearch disabled={mode === "readonly"} />
+      <div className="hidden md:block w-px h-10 bg-slate-300" />
+      <SearchBox disabled={mode === "readonly"} />
+    </div>
+  );
+};
